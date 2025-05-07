e141043b0fb6a988ec86d1107a3ada33c87716e8
JesusMRS05 <jususmrey@gmail.com>
2025-05-07 12:21:00 +0200

    refactor: create Action in mod package and repurpose Instruction

--- src/main/java/com/github/jesusmrs05/mcforgecommander/common/Instruction.java
+++ src/main/java/com/github/jesusmrs05/mcforgecommander/common/Instruction.java
@@ -1,37 +1,7 @@
 package com.github.jesusmrs05.mcforgecommander.common;
 
-import net.minecraftforge.client.event.InputUpdateEvent;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import java.io.Serializable;
 
-public enum Instruction implements Serializable, Runnable {
-	TOGGLE_MOVE_FORWARD {
-		private boolean isMoving = false;
-		private final Object movementListener = new Object() {
-			@SubscribeEvent
-			public void onInputUpdate(InputUpdateEvent event) {
-				if (isMoving) {
-					event.getMovementInput().moveForward = 1.0F;
-				}
-			}
-		};
-
-		@Override
-		public void run() {
-			isMoving = !isMoving;
-			Logger logger = LogManager.getLogger("MCForgeCommander");
-
-			if (isMoving) {
-				MinecraftForge.EVENT_BUS.register(movementListener);
-				logger.info("Movimiento hacia adelante ACTIVADO");
-			} else {
-				MinecraftForge.EVENT_BUS.unregister(movementListener);
-				logger.info("Movimiento hacia adelante DESACTIVADO");
-			}
-		}
-	};
+public enum Instruction implements Serializable {
+    TOGGLE_MOVE_FORWARD;
 }
