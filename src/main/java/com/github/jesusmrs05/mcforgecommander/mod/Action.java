64627636701e8ca2b2893cd7c770b207267d442b
JesusMRS05 <jususmrey@gmail.com>
2025-05-07 13:32:30 +0200

    feat(instruction): add toggle movement functionality

--- src/main/java/com/github/jesusmrs05/mcforgecommander/mod/Action.java
+++ src/main/java/com/github/jesusmrs05/mcforgecommander/mod/Action.java
@@ -6,8 +6,6 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.Serializable;
-
 public enum Action implements Runnable {
 	TOGGLE_MOVE_FORWARD {
 		private boolean isMoving = false;
@@ -31,6 +29,84 @@ public enum Action implements Runnable {
 			} else {
 				MinecraftForge.EVENT_BUS.unregister(movementListener);
 				logger.info("Movimiento hacia adelante DESACTIVADO");
+			}
+		}
+	},
+
+	TOGGLE_MOVE_BACKWARD {
+		private boolean isMoving = false;
+		private final Object movementListener = new Object() {
+			@SubscribeEvent
+			public void onInputUpdate(InputUpdateEvent event) {
+				if (isMoving) {
+					event.getMovementInput().moveForward = -1.0F;
+				}
+			}
+		};
+
+		@Override
+		public void run() {
+			isMoving = !isMoving;
+			Logger logger = LogManager.getLogger("MCForgeCommander");
+
+			if (isMoving) {
+				MinecraftForge.EVENT_BUS.register(movementListener);
+				logger.info("Movimiento hacia atrás ACTIVADO");
+			} else {
+				MinecraftForge.EVENT_BUS.unregister(movementListener);
+				logger.info("Movimiento hacia atrás DESACTIVADO");
+			}
+		}
+	},
+
+	TOGGLE_MOVE_LEFT {
+		private boolean isMoving = false;
+		private final Object movementListener = new Object() {
+			@SubscribeEvent
+			public void onInputUpdate(InputUpdateEvent event) {
+				if (isMoving) {
+					event.getMovementInput().moveStrafe = 1.0F;
+				}
+			}
+		};
+
+		@Override
+		public void run() {
+			isMoving = !isMoving;
+			Logger logger = LogManager.getLogger("MCForgeCommander");
+
+			if (isMoving) {
+				MinecraftForge.EVENT_BUS.register(movementListener);
+				logger.info("Movimiento a la izquierda ACTIVADO");
+			} else {
+				MinecraftForge.EVENT_BUS.unregister(movementListener);
+				logger.info("Movimiento a la izquierda DESACTIVADO");
+			}
+		}
+	},
+
+	TOGGLE_MOVE_RIGHT {
+		private boolean isMoving = false;
+		private final Object movementListener = new Object() {
+			@SubscribeEvent
+			public void onInputUpdate(InputUpdateEvent event) {
+				if (isMoving) {
+					event.getMovementInput().moveStrafe = -1.0F;
+				}
+			}
+		};
+
+		@Override
+		public void run() {
+			isMoving = !isMoving;
+			Logger logger = LogManager.getLogger("MCForgeCommander");
+
+			if (isMoving) {
+				MinecraftForge.EVENT_BUS.register(movementListener);
+				logger.info("Movimiento a la derecha ACTIVADO");
+			} else {
+				MinecraftForge.EVENT_BUS.unregister(movementListener);
+				logger.info("Movimiento a la derecha DESACTIVADO");
 			}
 		}
 	};
